0e0d39e7daf9caad3c6ae75417dae5a162982b46
Larry Burris <lburris@mac.com>
2021-11-30 00:34:07 -0500

    Initial Commit

---
+++ DynamicFetchRequest/DynamicFetchRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFetchRequestApp.swift
+//  DynamicFetchRequest
+//
+//  Created by Larry Burris on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFetchRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
